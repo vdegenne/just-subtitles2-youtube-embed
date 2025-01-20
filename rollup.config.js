56710dc7300517e141d8af86da7ec5e489a0fea4
Valentin Degenne <vdegenne@gmail.com>
2025-01-20 11:41:01 +0100

    v2

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import nodeResolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
+import nodeResolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 
 export default {
-  input: 'src/content.ts',
-  output: { file: 'content.js', format: 'es' },
-  plugins: [nodeResolve(), typescript()]
-}
+	input: './lib/content.js',
+	output: {file: './content.js', format: 'es'},
+	plugins: [nodeResolve(), terser()],
+};
--- src/content.ts
+++ src/content.ts
@@ -1,58 +1,54 @@
-import { copyContentSvg } from './copy_content_svg'
-import { copyAllLines, copyLastLine } from './util';
-
-function getElementFromSelector(selector: string, timeoutMs = 5000): Promise<HTMLElement> {
-  return new Promise(async (resolve, reject) => {
-    let theElement, resolved = false
-    setTimeout(() => { reject(); resolved = true }, timeoutMs)
-    while (!resolved && !(theElement = document.querySelector(selector))) {
-      await new Promise(r => setTimeout(r, 250))
-    }
-    resolve(theElement as HTMLElement)
-  })
-}
+import {getElement} from 'html-vision';
+import {copyContentSvg} from './copy_content_svg.js';
+import {copyAllLines, copyLastLine} from './util.js';
 
 try {
-
-  const rightControls = document.body.querySelector('.ytp-right-controls')
-  const button = document.createElement('button')
-  button.classList.add('ytp-button')
-  button.innerHTML = copyContentSvg
-  let pressed = false
-  let longPressTimeout: NodeJS.Timeout|undefined = undefined
-  function clearLongPressTimeout () { clearTimeout(longPressTimeout); longPressTimeout = undefined }
-  button.addEventListener('pointerdown', () => {
-    pressed = true
-    longPressTimeout = setTimeout(() => {
-      if (pressed) {
-        copyAllLines()
-        pressed = false
-        clearLongPressTimeout()
-      }
-    }, 1000)
-  })
-  button.addEventListener('pointerup', () => {
-    if (longPressTimeout) {
-      // timeout didn't fire, it's a common click
-      clearLongPressTimeout()
-      copyLastLine()
-    }
-    pressed = false
-  })
-  button.addEventListener('mouseleave', () => {
-    // cancel everything
-    pressed = false
-    clearLongPressTimeout()
-  })
-  rightControls?.prepend(button)
-
-} catch (e) { /* ignore */ }
+	const rightControls = document.body.querySelector('.ytp-right-controls');
+	const button = document.createElement('button');
+	button.classList.add('ytp-button');
+	button.innerHTML = copyContentSvg;
+	let pressed = false;
+	let longPressTimeout: number | undefined = undefined;
+	function clearLongPressTimeout() {
+		clearTimeout(longPressTimeout);
+		longPressTimeout = undefined;
+	}
+	button.addEventListener('pointerdown', () => {
+		pressed = true;
+		longPressTimeout = setTimeout(() => {
+			if (pressed) {
+				copyAllLines();
+				pressed = false;
+				clearLongPressTimeout();
+			}
+		}, 1000);
+	});
+	button.addEventListener('pointerup', () => {
+		if (longPressTimeout) {
+			// timeout didn't fire, it's a common click
+			clearLongPressTimeout();
+			copyLastLine();
+		}
+		pressed = false;
+	});
+	button.addEventListener('mouseleave', () => {
+		// cancel everything
+		pressed = false;
+		clearLongPressTimeout();
+	});
+	rightControls?.prepend(button);
+} catch (e) {
+	/* ignore */
+}
 
 (async () => {
-  try {
-    const moreVideosCloseButton = await getElementFromSelector('[aria-label="Hide more videos"]', 1000000)
-    moreVideosCloseButton.click()
-    const moreVideos = await getElementFromSelector('.ytp-pause-overlay', 10000000)
-    moreVideos.remove()
-  } catch (e) {}
-})()
+	try {
+		const moreVideosCloseButton = await getElement(
+			'[aria-label="Hide more videos"]',
+			10_000,
+		);
+		moreVideosCloseButton.click();
+		const moreVideos = await getElement('.ytp-pause-overlay', 10_000);
+		moreVideos.remove();
+	} catch (e) {}
+})();
